b84436ba6f2e3e20660e2b9e43bd51191dfdb1e2
Tiger1994 <1300288499@qq.com>
2019-06-03 10:59:53 +0800

    100

--- main_lapsrn.py
+++ main_lapsrn.py
@@ -17,14 +17,14 @@ from tqdm import tqdm
 # Training settings
 parser = argparse.ArgumentParser(description="PyTorch LapSRN")
 parser.add_argument("--batchSize", type=int, default=64, help="training batch size")
-parser.add_argument("--nEpochs", type=int, default=100, help="number of epochs to train for")
+parser.add_argument("--nEpochs", type=int, default=200, help="number of epochs to train for")
 parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
 parser.add_argument("--step", type=int, default=100, help="Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=10")
 parser.add_argument("--cuda", action="store_true", help="Use cuda?")
 parser.add_argument("--resume", default="", type=str, help="Path to checkpoint (default: none)")
 parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
 parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use, Default: 1")
-parser.add_argument("--momentum", default=0.9, type=float, help="Momentum, Default: 0.9")
+parser.add_argument("--momentum", default=0.5, type=float, help="Momentum, Default: 0.9")
 parser.add_argument("--weight-decay", "--wd", default=1e-4, type=float, help="weight decay, Default: 1e-4")
 parser.add_argument("--pretrained", default="", type=str, help="path to pretrained model (default: none)")
 
@@ -53,7 +53,8 @@ def main():
     if cuda and not torch.cuda.is_available():
         raise Exception("No GPU found, please run without --cuda")
 
-    opt.seed = random.randint(1, 10000)
+    # opt.seed = random.randint(1, 10000)
+    opt.seed = 0
     print("Random Seed: ", opt.seed)
     torch.manual_seed(opt.seed)
     if cuda:
--- result_comp.py
+++ result_comp.py
@@ -2,20 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+    font = {'family': 'Times New Roman',
+            'style': 'normal',
+            'weight': 'normal',
+            'color': 'black',
+            'size': 12
+            }
+    plt.rc('font', family='Times New Roman')
     plt.figure()
-    result0_1 = pd.read_csv('results0.1.csv')
+    result0_1 = pd.read_csv('results3.0.csv')
     data0_1 = result0_1['psnr']
     curve0_1 = plt.plot(data0_1,label='Original')
 
-    result1_1 = pd.read_csv('results1.1.csv')
+    result1_1 = pd.read_csv('results3.2.csv')
     data1_1 = result1_1['psnr']
     curve1_1 = plt.plot(data1_1, label='Add Cat')
 
-    result1_3 = pd.read_csv('results1.3.csv')
+    result1_3 = pd.read_csv('results3.1.csv')
     data1_3 = result1_3['psnr']
     curve1_3 = plt.plot(data1_3, label='Add cross space')
 
-    plt.xlabel('Epoch')
-    plt.ylabel('PSNR')
+    result1_2 = pd.read_csv('results3.3.csv')
+    data1_2 = result1_2['psnr']
+    curve1_2 = plt.plot(data1_2, label='Add long residual1')
+
+    # result1_4 = pd.read_csv('results1.4.csv')
+    # data1_4 = result1_4['psnr']
+    # curve1_4 = plt.plot(data1_4, label='Add long residual12')
+
+    plt.xlabel('Epoch', fontdict=font)
+    plt.ylabel('PSNR', fontdict=font)
     plt.legend(loc='lower right')
     plt.show()
